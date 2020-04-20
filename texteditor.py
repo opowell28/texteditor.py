3a25315eaa5f434742fd5ca216e5ddc22da3857b
Owen <opowell2899@gmail.com>
2020-04-19 23:49:40 -0400

    Modified to run on Python 2.7 due to compatibility issues with tkinter.  Also added code for save button.

--- texteditor.py
+++ texteditor.py
@@ -1,6 +1,22 @@
 import sys
-from tkinter import *
+from Tkinter import *
+import tkFileDialog
 
 root=Tk("Text Editor")
+
+text=Text(root)
+text.grid()
+
+def saveas():
+    global text
+
+    t = text.get("1.0", "end-1c")
+    saveLocation=tkFileDialog.asksaveasfilename()
+    file1=open(saveLocation, "w+")
+    file1.write(t)
+    file1.close()
+
+button=Button(root, text="Save", command=saveas)
+button.grid()
 
 root.mainloop()
